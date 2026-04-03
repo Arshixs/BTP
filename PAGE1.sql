1112182791dfbbada328fdca6e31bd8f0fc7967f
Arsh Natani <arshnatani123456@gmail.com>
2026-04-04 03:26:03 +0530

    First commit
    1. All data
    2. data processing ipynb file
    3. xgboost model code (simple plus lag based)

---
+++ PAGE1.sql
@@ -0,0 +1,33 @@
+Market Transaction:
+	Arrival volume: available
+	Modal Price: available
+	Max-Min Price: ?
+	Variety Type: will be treated differently anyway
+	Mandi location: if district location then available otherwise have to check
+	
+	
+Agro-Meteorological (Environmental) Data
+
+	Solar Radiation Indices:
+	temperature_2m	
+	relative_humidity_2m	
+	dew_point_2m	
+	apparent_temperature	
+	precipitation	
+	rain	
+	snowfall	
+	snow_depth	
+	pressure_msl	
+	surface_pressure	
+	cloud_cover	
+	cloud_cover_low	
+	cloud_cover_mid	
+	cloud_cover_high	
+	wind_speed_10m	
+	wind_speed_100m	
+	wind_direction_10m	
+	wind_direction_100m	
+	wind_gusts_10m
+	
+
+
